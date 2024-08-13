f0b1b43e55d4e70300a8c64f033ad35c4afcdc05
Rifat Bicer <rifatbicer00@gmail.com>
2024-08-14 01:17:15 +0300

    test

---
+++ js/demos/demo-creative-agency-2.js
@@ -0,0 +1,18 @@
+/*
+Name:           Demo Creative Agency 2
+Written by:     Okler Themes - (http://www.okler.net)
+Theme Version:  10.2.0
+*/
+
+(function( $ ) {
+	
+	'use strict';
+
+	$(window).on('load', function () {
+		setTimeout(function () {
+			$('.custom-hero-bg').addClass('loaded');
+		}, 500);
+	
+	});
+
+}).apply( this, [ jQuery ]);
